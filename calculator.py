258ada53f3aab9ed17614269d9a6897e325adbdf
Justoaws <jmebodo0703@yahoo.fr>
2024-09-21 10:12:28 -0400

    add new python code and output file

---
+++ calculator.py
@@ -0,0 +1,16 @@
+# write a program that will ask for two numbers (integer an float) and calculate the sum
+# of those tzo numbers.
+# display on the screen : the sum of number1 and number2 is: sum
+
+number1 = int(input("what is my age: "))
+
+number2 = float(input("what is my weight: "))
+
+sum = number1 + number2
+
+print(f"the sum of {number1} and {number2} is: {sum}.")
+
+#print("i'm "+str(number1)+" years old" " " "and" " " "i'm "+str(number2)+" kg")
+
+
+
---
+++ convertor.py
@@ -0,0 +1,15 @@
+# write a program that will ask for a user body weight in pound (lbs) and convert it
+# in kilogram (kg)
+# display on the screen: your body weight is: weight in lbs and the equivalent in kgs is
+# : weight in kg
+
+weight_lbs = 129.1027
+
+conversion = 0.453592
+
+weight_kg = weight_lbs * conversion
+
+print(f"my body weight is: {weight_lbs:.3f} in lbs and the equivalent in kgs is {weight_kg:.3f}")
+
+
+
