71fc3d5d46b17edf7facad957ef535f5d71b2502
Justoaws <jmebodo0703@yahoo.fr>
2024-10-03 18:36:14 -0400

    added files

---
+++ list-python-labs.py
@@ -0,0 +1,34 @@
+# Write a python program that prints the largest and smallest values in a lits
+# Print the two values on the same line, separated by a space
+# The largest value should appear first and the smallest value should appear second
+# You may assume that the first list only contains numeric values
+# If the list is empty, print "None"
+
+list_of_number1 = [3, 4, 5, 6]
+
+max = list_of_number1[3] 
+min = list_of_number1[0]
+
+if max <= 6 and min >= 3:
+    print(f'{max} {min}')
+
+
+list_of_number2 = [-1, -2, -3, -4]
+
+max = list_of_number2[0]
+min = list_of_number2[3]
+if max <= -1 and min >= -4:
+    print(f'{max} {min}')
+
+
+list_of_number3 = [0, 0, 0, 0]
+
+max = list_of_number3[0]
+min = list_of_number3[3]
+if max <= 0 and min >= 0:
+    print(f'{max} {min}')
+
+list_of_number = []
+
+if list_of_number == []:
+    print(list_of_number)
